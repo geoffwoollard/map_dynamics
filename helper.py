d6d239e67460111c757926c1ae03b11bbe54cc95
Geoffrey Woollard <geoffwoollard@gmail.com>
2019-09-02 12:14:42 -0400

    rint to floor to get rid of missing stripe

--- helper.py
+++ helper.py
@@ -2,11 +2,16 @@ import numpy as np
 
 # TODO: error in no values going to middie (coordinates with zero, rounding error?)
 def coord_to_index(coord,offset,dx):
-    index = np.rint((coord - offset)/dx).astype(int)
+    index = np.floor((coord - offset)/dx).astype(np.int32)
     return(index)
 
 
-def coords_to_map(coords,vals,offset = -128,dx=2,N=128):
+def coords_to_map(coords,vals,offset,dx,N):
+    '''
+    offset: side/2
+    dx: pixel step size. side/float(int(side / (side/nsamples)))
+    N: number of pixels along an axis.
+    '''
     coarse_map = np.zeros((N,N,N))
     for coord,val in zip(coords,vals):
         x,y,z = coord_to_index(coord,offset=offset,dx=dx).tolist()

aa25e3e0a6077e5bd055c1ded7f5d14edb22cc47
Geoffrey Woollard <geoffwoollard@gmail.com>
2019-10-17 22:08:41 -0400

    blocks working with sparse eig (not GPU). recapitulate h_anm with ss blocks

--- blocks.py
+++ blocks.py
@@ -1,6 +1,7 @@
 # based on https://github.com/prody/ProDy/blob/e66e29f8d5368d40ce51b89eb05fa3d91fd18f77/prody/dynamics/rtb.py
 
 import numpy as np
+import scipy
 
 class Increment(object):
 
@@ -56,11 +57,41 @@ def calc_projection_setup(coords, blks, natoms):
 
     return(coords, blks, project, natoms, nblocks, nb6, maxsize)
 
-def do_projection(hessian,coords, blks, project, natoms, nblocks, nb6, maxsize):
-
+def get_projection(coords, blks, project, natoms, nblocks, nb6, maxsize):
     from blocksmodule import calc_projection # TODO: from .blocksmodule
+    calc_projection(coords, blks, project, natoms, nblocks, nb6, maxsize) # returns project filled
+    return(project)
 
-    calc_projection(coords, blks, project, natoms, nblocks, nb6, maxsize)
+def nan_to_zero(arr):
+    nans = np.isnan(arr)
+    nans_sum=nans.sum()
+    if nans_sum > 0:
+        arr[nans] = 0
+        print('set %i nans in projection matrix to zero'%nans_sum)
+    return(arr)
 
-    hessian_block = project.T.dot(hessian).dot(project)
+def do_projection(hessian,coords, blks, project, natoms, nblocks, nb6, maxsize):
+    '''
+    TODO: debug
+    '''
+    project = get_projection(coords, blks, project, natoms, nblocks, nb6, maxsize)
+    project = nan_to_zero(project)
+    hessian_block = np.linalg.multi_dot([project.T,hessian,project])
     return(hessian_block,project)
+
+def make_sparse(arr):
+    return(scipy.sparse.lil_matrix(arr))
+
+def sparse_dot(hessian_anm_s,project_s):
+    '''ms instead of 2min for 27k x 27k with 0.5 % filled hessian_anm'''
+    hessian_blk_s = project_s.T * hessian_anm_s * project_s
+    return(hessian_blk_s)
+
+def do_sparse_dot_wrapper(hessian_anm,project):
+    hessian_anm_s = make_sparse(hessian_anm)
+    project_s = make_sparse(project)
+    hessian_blk_s = sparse_dot(hessian_anm_s,project_s)
+    hessian_blk_s_d = hessian_blk_s.dense()
+    return(hessian_blk_s_d)
+
+
--- eig.py
+++ eig.py
@@ -1,17 +1,33 @@
 import pycuda.gpuarray as gpuarray
-import pycuda.autoinit
 import numpy as np
-from skcuda import linalg
+import scipy
 
-linalg.init() # TODO go inside function?
+def gpu_setup():
+    import pycuda.autoinit
+    from skcuda import linalg
+    linalg.init() # TODO go inside function?
 
-def hess_to_eig(hess,nmodes=20, shift=6):
-  #TODO add nmodes and shift
-  assert max(hess.shape) < 50000, 'RAM maxes out ~ 20000'
-  hess = np.array(hess, np.float32, order='F')
-  hess_gpu = gpuarray.to_gpu(hess)
-  vec_gpu, val_gpu = linalg.eig(hess_gpu, 'N', 'V')
-  vec,val = vec_gpu.get().T, val_gpu.get()
-  vec = vec[:,shift:shift+nmodes]
-  val = val[shift:shift+nmodes]
-  return(vec,val)
+def hess_to_eig_gpu(hess,nmodes=20, shift=6):
+    assert max(hess.shape) < 50000, 'RAM maxes out ~ 20000'
+    hess = np.array(hess, np.float32, order='F')
+    hess_gpu = gpuarray.to_gpu(hess)
+    vec_gpu, val_gpu = linalg.eig(hess_gpu, 'N', 'V')
+    vec,val = vec_gpu.get().T, val_gpu.get()
+    vec = vec[:,shift:shift+nmodes]
+    val = val[shift:shift+nmodes]
+    return(vec,val)
+
+def blk_hess_to_eig(hess,project,nmodes=20,shift=6):
+    scipy.linalg.eigh
+    vals,blk_vecs = scipy.linalg.eigh(
+        hess,
+        eigvals=(0,nmodes+shift-1)
+        )
+    vecs = np.dot(project,blk_vecs[:,shift:])
+    vals=vals[shift:]
+    return(vecs,vals)
+
+def sparse_hess_to_eig(hess_s,nmodes=20,shift=6,which='SM'):
+    vals, vecs = scipy.sparse.linalg.eigsh(hess_s,k=nmodes+shift,which=which) # because h_blk_s symmetric
+    # TODO: remove zeros once figure out if before or after
+    return(vals, vecs)

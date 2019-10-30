6d91f07789cd97b815ebb355b1a028c78c995014
Geoffrey Woollard <geoffwoollard@gmail.com>
2019-10-30 00:20:40 -0400

    sparse hessian. 8.9x slowdown for 8.8k nodes, not counting need to sparsely encode dense after computing

--- hessian.py
+++ hessian.py
@@ -1,16 +1,22 @@
 import numpy as np
-from numba import jit
+import scipy
+#from numba import jit
 
-@jit
-def hess_from_coords(coords,cutoff=15.,gamma=1.):
-    #TODO: sparse scipy
+#@jit
+def hess_from_coords(coords,cutoff=15.,gamma=1.,sparse=True):
+    '''
+    sparse: encoding the hessian as sparse is very important since the final matrix will be sparse. suitable for very large matrices since the memory won't swamp with a large sparse matrix
+    '''
     assert coords.shape[1] == 3, 'coords shape is not Nx3'
     dof = coords.size
     n_atoms = coords.shape[0]
     assert np.isclose(dof / 3.,n_atoms)
     cutoff2 = cutoff * cutoff
     g = gamma # TODO: function for distance dependence / scalar map value
-    hessian = np.zeros((dof, dof), np.float32)
+    if sparse:
+        hess = np.zeros((dof, dof), np.float32)
+    else:
+        hess = scipy.sparse.lil_matrix((dof, dof),dtype=np.float32) # lil_matrix good for the indexing in the loop. csr throws warning
     for i in range(n_atoms):
         res_i3 = i*3
         res_i33 = res_i3+3
@@ -26,11 +32,11 @@ def hess_from_coords(coords,cutoff=15.,gamma=1.):
             res_j33 = res_j3+3
             super_element = np.outer(i2j, i2j) * (- g / dist2)
             #TODO: try to vectorize local transpose symetry
-            hessian[res_i3:res_i33, res_j3:res_j33] = super_element
-            hessian[res_j3:res_j33, res_i3:res_i33] = super_element
+            hess[res_i3:res_i33, res_j3:res_j33] = super_element
+            hess[res_j3:res_j33, res_i3:res_i33] = super_element
             # TODO: test vectorized summing up rows/cols outside of j loop
-            hessian[res_i3:res_i33, res_i3:res_i33] = \
-                hessian[res_i3:res_i33, res_i3:res_i33] - super_element
-            hessian[res_j3:res_j33, res_j3:res_j33] = \
-                hessian[res_j3:res_j33, res_j3:res_j33] - super_element
-    return(hessian)
+            hess[res_i3:res_i33, res_i3:res_i33] = \
+                hess[res_i3:res_i33, res_i3:res_i33] - super_element
+            hess[res_j3:res_j33, res_j3:res_j33] = \
+                hess[res_j3:res_j33, res_j3:res_j33] - super_element
+    return(hess)

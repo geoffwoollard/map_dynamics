71bc8b005bdd05e30d411f02c18175de61ad33d6
Geoffrey Woollard <geoffwoollard@gmail.com>
2019-12-23 19:20:36 -0500

    calculate projection written in python

--- blocks.py
+++ blocks.py
@@ -76,6 +76,163 @@ def get_projection(coords, blks, project, natoms, nblocks, nb6, maxsize):
     calc_projection(coords, blks, project, natoms, nblocks, nb6, maxsize) # returns project filled
     return(project)
 
+def calc_projection_py(coords, blks, project, natoms, nblocks, nb6, maxsize,method='csr'):
+    '''
+    blocksmodule:calc_projection implemented in python
+    # TODO optimize by 
+        vectorizing more
+        benchmarking svd vs eigh
+        sparse output 
+        hsize check redundant
+        IC no need for copy
+        HH vs PP same thing
+    '''
+    bmx = maxsize
+    bdim = nb6
+    nblx = nblocks
+    natm = natoms
+
+    if 18*bmx*nblx > 12*natm:
+        hsize=12*natm
+    else:
+        hsize=18*bmx*nblx
+
+    hh_idx = np.zeros((hsize,2))
+    hh_x = np.zeros(hsize)
+    pp_idx = np.zeros_like(hh_idx,dtype=np.int32)
+    pp_x = np.zeros_like(hh_x,dtype=np.float64)
+    elm,pp_idx,pp_x = dblock_projections2_py(coords,blks,pp_idx,pp_x,natm,nblx,bmx)
+    project = copy_prj_ofst_py(project,elm,pp_idx,pp_x,bdim,method=method)
+    return(project)
+ 
+
+def dblock_projections2_py(coords,blks,pp_idx,pp_x,natm,nblx,bmx):
+    nres=natm
+    #/* INITIALIZE BLOCK ARRAYS */
+    elm = 0;
+    #X = np.zeros((bmx,3))# dmatrix(1, bmx, 1, 3);
+    IDX = np.zeros(bmx,dtype=np.int32)#ivector(1, bmx);
+    #CM = np.zeros(3)#dvector(1, 3);
+    #I = np.zeros((3,3))#dmatrix(1, 3, 1, 3);
+    #IC = np.zeros((3,3))#dmatrix(1, 3, 1, 3);
+    #W = np.zeros(3)#dvector(1, 3);
+    #A = np.zeros((3,3))#dmatrix(1, 3, 1, 3);
+    ISQT = np.zeros((3,3))#dmatrix(1, 3, 1, 3);
+
+    # cycle through blocks
+    # TODO: verify works for blocks of size 1 (zero entries, averages, etc)
+    for b in range(1,nblx+1):
+        # clear matrices
+        CM = np.zeros(3)
+        I = np.zeros((3,3))
+        X = np.zeros((bmx,3))
+        
+        # store values for current block
+        nbp=0
+        for i in range(nres):
+            if blks[i] == b:
+                nbp+=1
+                IDX[nbp-1]=i 
+                X[nbp-1]=coords[:,i]
+                CM = CM+coords[:,i]
+        
+        # translate block centre of mass to origin
+        CM = CM/nbp
+        X=X-CM
+        
+        # TODO: condition on nbp > 1 because do not use I, W, A, ISQT for blocks of one
+        # calculate inertia tensor
+        for k in range(nbp):
+            dd=0
+    #         df = X[k]
+            dd=(X[k]*X[k]).sum() # or np.linalg.norm(X[k])**2
+            for i in range(3):
+                I[i,i] +=  dd - X[k,i]*X[k,i]
+                for j in range(i+1,3):
+                    I[i,j] -= X[k,i]*X[k,j]
+                    I[j,i] = I[i,j]
+        
+        # diagonalize inertia tensor
+        IC = I.copy()
+        # blocksmodule : dsvd (a, w, v) --> (U W Vt) in http://numerical.recipes/webnotes/nr3web2.pdf --> (u,s,vh) in np.linalg.svd
+    #     u, s, vh = np.linalg.svd(IC, full_matrices=False)#,hermitian=True) # s sorted descending
+    #     W = s 
+    #     A = vh.T # transpose corresponds to column eigenvectors in scipy.linalg.eigh
+
+        W,A = scipy.linalg.eigh(IC) # same as svd, check which faster on 3x3 matrices
+        cp = np.cross(A[:,0],A[:,1]) 
+        if np.dot(cp,A[:,2]) < 0:
+            A[:,2] = - A[:,2]
+        
+        # find its square root
+        for i in range(3):
+            for j in range(3):
+                dd=0
+                for k in range(3):
+                    dd+=A[i,k]*A[j,k]/np.sqrt(W[k]) # divide by zero!
+                ISQT[i,j]=dd
+
+        # update pp with the rigid motions of the block
+        tr=nbp**-.5
+        
+        for i in range(nbp): 
+            
+            # /* TRANSLATIONS: 3*(IDX[i]-1)+1 = x-COORDINATE OF RESIDUE IDX[i]; 6*(b-1)+1 = x-COORDINATE OF BLOCK b */
+            # ie x,y,z coord have j=0,1,2
+            for j in range(3):
+                pp_idx[elm,0] = 3*(IDX[i])+j # IDX contains indeces that start at 0
+                pp_idx[elm,1] = 6*(b-1)+j # blocks start at 1 
+                pp_x[elm] = tr
+                elm+=1
+
+            # rotations
+            if nbp > 1: # WARNING: bad values for nbp=2 because near zero eigenvector and blows up when divide by it
+                for ii in range(3):
+                    for jj in range(3):
+                        if jj == 0:
+                            aa=2
+                            bb=3
+                        elif jj==1:
+                            aa=3
+                            bb=1
+                        else:
+                            aa=1
+                            bb=2
+                        dd = ISQT[ii][aa-1]*X[i][bb-1] - ISQT[ii][bb-1]*X[i][aa-1]
+                        #print('i,ii,jj,dd',i,ii,jj,dd)
+                        pp_idx[elm,0] = 3*(IDX[i])+jj
+                        pp_idx[elm,1] = 6*(b-1)+3+ii
+                        pp_x[elm] = dd
+                        elm += 1
+                        # pp_idx[elm-1] is the last item in pp_idx[elm-1], zeros from pp_idx[elm] onwards
+    return(elm,pp_idx[:elm],pp_x[:elm])
+        
+def copy_prj_ofst_py(project,elm,pp_idx,pp_x,bdim,method):
+    '''
+    project: empty 2d array
+    '''
+    mx = pp_idx[:,1].max()+1
+    I1 = np.zeros(mx,np.int32)
+    I2 = np.zeros(mx,np.int32)
+    for i in range(elm):
+        I1[pp_idx[i,1]] = pp_idx[i,1]+1 # have to start indexing at one bc check for zero in code right after. subtract one later once transformed
+    j=0
+    for i in range(mx):
+        if I1[i] != 0: j+= 1
+        I2[i]=j
+    I2 -= 1 # correct from before
+    if method == 'reshape':
+        assert False #TODO: problem with zero values not being zero. atol 1e-2 not 1e-3 (not good enough)
+        project_1d = project.flatten()
+        for i in range(elm):
+             if pp_x[i] != 0:
+                    project_1d[bdim*pp_idx[i,0] + I2[pp_idx[i,1]]] = pp_x[i] # no minus one since zero indexed
+        project = project_1d.reshape(project.shape)
+    elif method == 'csr':
+        proj_csr = scipy.sparse.csr_matrix((pp_x, (pp_idx[:,0], I2[pp_idx[:,1]])))
+        project = proj_csr
+    return(project)
+    
 def nan_to_zero(arr):
     nans = np.isnan(arr)
     nans_sum=nans.sum()
